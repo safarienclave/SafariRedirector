317969cf565d759bdddf8ddfafb2d143d7e14637
gulshadafzalkhan@gmail.com <gulshadafzalkhan@gmail.com>
2022-11-12 03:11:19 +0500

    Add project files.

---
+++ README.md
@@ -0,0 +1,4 @@
+# SafariEnclaveExpressApp
+
+
+"# SafariEnclaveExpressApp" 
---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require("express");
+var cors = require("cors");
+var bodyParser = require("body-parser");
+
+var app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/", (request, response) => {
+    response.redirect(`http://fileverification.safarienclavehousing.com/OTPVerification.php?Id=${request.query.Id}`);
+    //add tnhis
+    //OTPVerification.php?Id=${request.query.Id}
+});
+
+app.listen(port, () => {
+    console.log("Server is running on port: ." + port)
+})
